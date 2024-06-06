ea5713f77343e4e0a3890cbd6e4435b48b0a9d54
Pankaj Sahu <81400644+pankajsao11@users.noreply.github.com>
2024-06-06 19:37:26 +0530

    Deleted terraform_modules directory

--- terraform_modules/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket               = "bucket-name"
-    key                  = "foldername/terraform.tfstate" #bucket>folder>tfstate file
-    region               = "us-east-1"                    #put your required region here
-    dynamodynamodb_table = "table-name"                   #put your table name here   
-  }
-}
